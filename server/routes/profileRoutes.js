5709a9153e5a461a2786c98ea5c1668566d3911f
praka <prakashswami150569@gmail.com>
2025-09-22 16:05:37 +0530

    added profiles routes

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -1,11 +1,13 @@
-// routes/patientRoutes.js
+
 import express from 'express';
-import { getPatientById, updatePatient } from '../controllers/profileController.js';
+import { getPatientById, updatePatient, getUserBookings, getUserRecords } from '../controllers/profileController.js';
 
 const router = express.Router();
 
 // GET patient by ID
 router.get('/:userId', getPatientById);
 router.put('/updatePatient/:id', updatePatient);
+router.get("/userBookings/:userId", getUserBookings);
+router.get("/userRecords/:userId", getUserRecords);
 
 export default router;
