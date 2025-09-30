64b3d49bd1a80a5014e56983cfadaf9e9fd49158
praka <prakashswami150569@gmail.com>
2025-09-30 15:04:17 +0530

    minor changes

--- server/mailTransporter.js
+++ server/mailTransporter.js
@@ -1,9 +1,10 @@
 import nodemailer from 'nodemailer';
 
+
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'prakashswami150569@gmail.com',       // your email
-        pass: 'vrrcgdzipkqqhcie'           // app password from Gmail
+        user: process.env.USER,       // your email
+        pass: process.env.PASSCODE          // app password from Gmail
     }
 });
